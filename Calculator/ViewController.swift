23fb1dfcc0085bb20b98517b002a963e45da65cc
leon <526247082@qq.com>
2018-11-02 16:30:37 +0800

    Setup

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,12 +9,63 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //optional ui label
+    @IBOutlet weak var display: UILabel!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    var userIsInTheMiddleOfTyping = false
+    
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
     }
-
-
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+//        print("\(digit) touchDigit was called!")
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display!.text!
+            display!.text = textCurrentlyInDisplay + digit
+        } else {
+            display!.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
+        
+    }
+    
+    
+    //option + p = pai; option + v = square root
+    @IBAction func performOperation(_ sender: UIButton) {
+        userIsInTheMiddleOfTyping = false
+        //if is a blank button, it will crash
+//        let mathematicalSymbol = sender.currentTitle!
+        if let mathematicalSymbol = sender.currentTitle {
+//            if mathematicalSymbol == "π" {
+//
+//            } else if {
+//
+//            }
+            switch mathematicalSymbol {
+            case "π":
+//                display!.text = String(Double.pi)
+                displayValue = Double.pi
+                break
+            case "√":
+//                let operand = Double(display!.text!)
+//                display!.text = String(sqrt(operand!))
+                displayValue = sqrt(displayValue)
+                break
+            default:
+                break
+            }
+        } else {
+            
+        }
+        
+    }
 }
 
